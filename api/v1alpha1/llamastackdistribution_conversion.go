466dcff705f574a9a05925a745fd2a0f0e477736
Vaishnavi Hire <vhire@redhat.com>
2026-02-25 21:20:27 +0530

    Add deployment files for webhook
    Signed-off-by: Vaishnavi Hire <vhire@redhat.com>

--- api/v1alpha1/llamastackdistribution_conversion.go
+++ api/v1alpha1/llamastackdistribution_conversion.go
@@ -21,12 +21,11 @@ import (
 	"fmt"
 	"strconv"
 
+	v1alpha2 "github.com/llamastack/llama-stack-k8s-operator/api/v1alpha2"
 	corev1 "k8s.io/api/core/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
-
-	v1alpha2 "github.com/llamastack/llama-stack-k8s-operator/api/v1alpha2"
 )
 
 const (
